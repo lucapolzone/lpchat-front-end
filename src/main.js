ffc8dd4a275808f5f177e940539656b2b9056f1d
lucapolzone <lucapolzone@gmail.com>
2024-12-07 14:47:35 +0100

    feat: start configuring Pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+import piniaPersist from 'pinia-plugin-persistedstate';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+pinia.use(piniaPersist); // plugin per la persistenza
+
+createApp(App)
+  .use(pinia) // <- aggiungo pinia
+  .mount('#app')
